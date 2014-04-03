ad464608bb1e4bc5261ab4c90485d50ab058e419
BALDE <bt008465@etu.unice.fr>
2014-04-03 12:47:54 +0200

    avant test heroku

---
+++ mon_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MonApp::Application.config.secret_key_base = '797d7587265190a2e969803361a5cb41ce9d675859db0ca16065e7a455422ef6f9788b2b0778bcfb43f82f2cd2fe98d33988a5ad2ae03c2192ca920822ea6222'
---
+++ mon_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MonApp::Application.config.session_store :cookie_store, key: '_mon_app_session'
